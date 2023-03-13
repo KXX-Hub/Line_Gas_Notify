5cf1a70e044a92f9656792bd2d7fa4c63c46e617
K <97007ken@gmail.com>
2023-03-14 00:14:49 +0800

    Update - fix notify message

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import time
+
 from etherscan import Etherscan
+
 import line_notify
 import utilities as utils
 
@@ -64,7 +66,7 @@ def get_gas_notify():
             else:
                 message = "Gas is out of safe range\n"
                 line_notify.send_message(message)
-                message =  "\n----------------------------------\n" \
+                message = "\n----------------------------------\n" \
                           f"Safe Gas : {safe_gas} Gwei\n" \
                           f"Proposed Gas : {proposed_gas} Gwei\n" \
                           f"Fast Gas : {fast_gas} Gwei\n" \
@@ -121,7 +123,7 @@ def get_gas_notify():
                           f"Fast Gas : {fast_gas} Gwei\n" \
                           f"----------------------------------\n" \
                           f"Suggest Base Fee : {suggest_base_fee} Gwei\n" \
-                          f"Your target gas : {target_gas} Gwei" \
+                          f"Your target gas : {target_gas} Gwei\n" \
                           f"----------------------------------"
                 line_notify.send_message(message)
                 time_counter['too_high_gas_msg'] = 0
